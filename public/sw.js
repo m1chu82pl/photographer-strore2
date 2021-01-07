fff943932199cbba3e27e2e2977079ea3f5ae9ff
m1chu82pl <criminal.industries@gmail.com>
2021-01-07 13:18:48 +0100

    es6

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,34 @@
 const cacheName = 'site-static-v2';
 const assets = [
     '/',
+    '/index.js',
     '/favicon.ico',
     '/logo192.png',
 ];
 
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
+self.addEventListener('install', e => {
+    console.log('ServiceWorker: Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
+        caches.open(cacheName).then(cache => {
+            console.log('ServiceWorker: Caching app shell');
             return cache.addAll(assets);
         })
     );
 });
 
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener("activate", function(e) {
-    console.log("[ServiceWorker] Activate");
+self.addEventListener("activate", e => {
+    console.log("ServiceWorker: Activ");
 });
 
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
         .then((response) => {
             if (response) {
-                console.log('h1', response);
+                console.log('response', response);
                 return response
             }
             return fetch(event.request)
@@ -35,17 +36,16 @@ self.addEventListener('fetch', (event) => {
                     if (!response || response.status !== 200 || response.type !== 'basic') {
                         return response;
                     }
-                    var responseToCache = response.clone();
                     caches.open(cacheName)
                         .then((cache) => {
-                            console.log('here', cache);
-                            cache.put(event.request, responseToCache);
+                            console.log('cache', cache);
+                            cache.put(event.request, response.clone());
                         });
                     return response;
                 })
         })
         .catch((error) => {
-            console.log('in catch', error);
+            console.log('fetch error', error);
         })
     );
 });
