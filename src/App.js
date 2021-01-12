76a0a3a7c1486adade6520d669108e6119524312
m1chu82pl <criminal.industries@gmail.com>
2021-01-12 12:40:56 +0100

    SW and firebase deactivated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import firebase from "./firebase";
+// import firebase from "./firebase";
+
+import Header from './components/Header'
 
 function App() {
 
   useEffect(() => {
-    firebase
-      .messaging()
-      .getToken()
-      .then((token) => {
-        console.log(Notification.permission);
-        if(Notification.permission !== 'granted') return;
-        firebase.database().ref('/tokens').push([
-          token,          
-        ]);
-        console.log(`token: ${token}`);})
-      .catch(() => console.log("user didn't give permission"));
+    // firebase
+    //   .messaging()
+    //   .getToken()
+    //   .then((token) => {
+    //     console.log(Notification.permission);
+    //     if(Notification.permission !== 'granted') return;
+    //     firebase.database().ref('/tokens').push([
+    //       token,          
+    //     ]);
+    //     console.log(`token: ${token}`);})
+    //   .catch(() => console.log("user didn't give permission"));
       
     }, []);
 
 
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
