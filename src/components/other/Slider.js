f6ad28e5a08cf319979cced23af4f55d1ebc138f
m1chu82pl <criminal.industries@gmail.com>
2021-01-20 14:20:09 +0100

    NO for this slider

--- src/components/other/Slider.js
+++ src/components/other/Slider.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Slide } from "react-slideshow-image";
-import "./SliderStyles.css";
 import "react-slideshow-image/dist/styles.css";
+// import "./SliderStyles.css";
+import styles from './Slider.module.scss'
 import slider1 from '../../assets/navigation/slider1.jpg';
 import slider2 from '../../assets/navigation/slider2.jpg';
 import slider3 from '../../assets/navigation/slider3.jpg';
@@ -10,7 +11,7 @@ const Slider = () => {
     const slideRef = React.createRef();
   
     const properties = {
-      duration: 5000,
+      duration: 3000,
       autoplay: true,
       transitionDuration: 500,
       arrows: true,
@@ -22,17 +23,15 @@ const Slider = () => {
     ];
 
     return (
-      <div className="App">
-        <div className="slide-container">
+        <div className={styles.slidesWrapper}>
           <Slide ref={slideRef} {...properties}>
             {slideImages.map((each, index) => (
-              <div key={index} className="each-slide">
+              <div key={index} className={styles.eachSlide}>
                 <img src={each} alt="sample" />
               </div>
             ))}
           </Slide>
         </div>
-      </div>
     );
 }
 
