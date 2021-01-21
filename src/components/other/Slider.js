153c63a6c96b2684716c260466ba4a0d026bcc8b
m1chu82pl <criminal.industries@gmail.com>
2021-01-21 10:33:33 +0100

    add styles to Slider arrows and more

--- src/components/other/Slider.js
+++ src/components/other/Slider.js
@@ -1,52 +1,64 @@
-import React, { useState } from 'react';
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import {
+  IoIosArrowDroprightCircle,
+  IoIosArrowDropleftCircle,
+} from "react-icons/io";
 import styles from "./Slider.module.scss";
 // import './Slider.css';
-
-
 
 import slider1 from "../../assets/navigation/slider1.jpg";
 import slider2 from "../../assets/navigation/slider2.jpg";
 import slider3 from "../../assets/navigation/slider3.jpg";
 
-  // const slideRef = React.createRef();
+// const slideRef = React.createRef();
 
-  const imagesArray = [slider1, slider2, slider3]
+const imagesArray = [slider1, slider2, slider3];
 
-  const ImageSlider = () => {
-    const [current, setCurrent] = useState(0);
-    const length = imagesArray.length;
-  
-    const nextSlide = () => {
-      setCurrent(current === length - 1 ? 0 : current + 1);
-    };
-  
-    const prevSlide = () => {
-      setCurrent(current === 0 ? length - 1 : current - 1);
-    };
-  
-    if (!Array.isArray(imagesArray) || length <= 0) {
-      return null;
-    }
-  
-    return (
+const ImageSlider = () => {
+  const [current, setCurrent] = useState(0);
+  const length = imagesArray.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(imagesArray) || length <= 0) {
+    return null;
+  }
+
+  return (
+    <IconContext.Provider value={{ className: `${styles.reactIcons}` }}>
       <section className={styles.slider}>
-        <FaArrowAltCircleLeft className={styles.leftArrow} onClick={prevSlide} />
-        <FaArrowAltCircleRight className={styles.rightArrow} onClick={nextSlide} />
+        <IoIosArrowDropleftCircle
+          className={styles.leftArrow}
+          onClick={prevSlide}
+        />
+        <IoIosArrowDroprightCircle
+          className={styles.rightArrow}
+          onClick={nextSlide}
+        />
         {imagesArray.map((slide, index) => {
           return (
             <div
-              className={index === current ? `${styles.slideActive}` : `${styles.slide}`}
+              className={
+                index === current ? `${styles.slideActive}` : `${styles.slide}`
+              }
               key={index}
             >
               {index === current && (
-                <img src={slide} alt='travel' className={styles.image} />
+                <img src={slide} alt="travel" className={styles.image} />
               )}
             </div>
           );
         })}
       </section>
-    );
-  };
-  
-  export default ImageSlider;
+    </IconContext.Provider>
+  );
+};
+
+export default ImageSlider;
