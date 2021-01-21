92384236019d405357c00ab78b8fe317d68f6f5b
m1chu82pl <criminal.industries@gmail.com>
2021-01-21 08:46:44 +0100

    add new slider with npm icons

--- src/components/other/Slider.js
+++ src/components/other/Slider.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 import styles from "./Slider.module.scss";
+
+
+
 import slider1 from "../../assets/navigation/slider1.jpg";
 import slider2 from "../../assets/navigation/slider2.jpg";
 import slider3 from "../../assets/navigation/slider3.jpg";
 
-const Slider = () => {
   // const slideRef = React.createRef();
 
-  return (
-    <div className={styles.eachSlide}>
-      <img src={slider1} alt="sample" />
-    </div>
-  );
-};
+  const imagesArray = [slider1, slider2, slider3]
 
-export default Slider;
+  const ImageSlider = ({ slides }) => {
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+  
+    const nextSlide = () => {
+      setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+  
+    const prevSlide = () => {
+      setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+  
+    if (!Array.isArray(slides) || slides.length <= 0) {
+      return null;
+    }
+  
+    return (
+      <section className='slider'>
+        <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+        <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+        {imagesArray.map((slide, index) => {
+          return (
+            <div
+              className={index === current ? 'slide active' : 'slide'}
+              key={index}
+            >
+              {index === current && (
+                <img src={slide} alt='travel' className='image' />
+              )}
+            </div>
+          );
+        })}
+      </section>
+    );
+  };
+  
+  export default ImageSlider;
