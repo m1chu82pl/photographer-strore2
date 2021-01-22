6162386c63f8a36d7496a101fd5d36262d75d29f
m1chu82pl <criminal.industries@gmail.com>
2021-01-22 13:58:06 +0100

    add actualPathName

--- src/components/main/ImagesGallery.js
+++ src/components/main/ImagesGallery.js
@@ -10,7 +10,10 @@ import styles from "./ImagesGallery.module.scss";
 
 const ImagesGallery = () => {
   const [isHidden, setIsHidden] = useState(true);
-  const [clickedImg, setClickedImg] = useState(null)
+  const [clickedImg, setClickedImg] = useState(null);
+
+  const actualPathName = window.location.pathname.slice(1);
+  console.log("actualPathName:  ", actualPathName);
 
   const handleShowImages = (index) => {
     setIsHidden(!isHidden);
@@ -18,21 +21,22 @@ const ImagesGallery = () => {
     setClickedImg(nature[index]);
   }
   
-  // console.log(isHidden);
+  // console.log(window);
+  // console.log(window.location.pathname.slice(1));
   // console.log("clickedImg ", typeof(clickedImg));
   return (
 <>
     {isHidden ? (
-    <div className={styles.natureWrapper}>
+    <div className={styles.natureWrapper} >
       {nature.map((natImg, index) => {
         return (
           <div className={styles.natureWrapperImg} key={index}>
-            <img src={natImg} className={styles.image} onClick={() => handleShowImages(index)} alt="nature" />
+            <img src={natImg} className={styles.image} onClick={() => handleShowImages(index)} alt={actualPathName} />
           </div>
         );
       })}
     </div>
-    ) : (<div className={styles.clickedImgWrapper}><img src={clickedImg} alt=""/></div>)}
+    ) : (<div className={styles.clickedImgWrapper}><img src={clickedImg} alt={window.location.pathname.slice(1)}/></div>)}
     </>
   );
 };
