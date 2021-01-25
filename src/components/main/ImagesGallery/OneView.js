cbdfa118deb0956945fed25c2d6019cf3da6a594
m1chu82pl <criminal.industries@gmail.com>
2021-01-25 15:31:22 +0100

    add arrows to OneView

--- src/components/main/ImagesGallery/OneView.js
+++ src/components/main/ImagesGallery/OneView.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import styles from "./ImagesGallery.module.scss";
 
-const OneView = ( {altName, clickedImg} ) => {
-  return ( <div className={styles.clickedImgWrapper}>
-    <img
-      src={clickedImg}
-      className={styles.clickedImg}
-      alt={altName}
-    />
-  </div> );
-}
- 
+const OneView = ({ altName, clickedImg }) => {
+  return (
+    <div className={styles.clickedImgWrapper}>
+      <div className={styles.prevImage}>
+        <div className={styles.prevImageArrow}> ⬅  </div>
+      </div>
+      <div className={styles.nextImage}>
+        <div className={styles.nextImageArrow}> ➡  </div>
+      </div>
+      <img src={clickedImg} className={styles.clickedImg} alt={altName} />
+    </div>
+  );
+};
+
 export default OneView;
