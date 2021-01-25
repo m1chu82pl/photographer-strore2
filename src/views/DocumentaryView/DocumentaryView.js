1261d94ffbc9c230c15121a616cf7bf85d679eb0
m1chu82pl <criminal.industries@gmail.com>
2021-01-25 14:56:07 +0100

    add styles and changes views in components

--- src/views/DocumentaryView/DocumentaryView.js
+++ src/views/DocumentaryView/DocumentaryView.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Navigation from "../../components/main/Navigation";
 import ImagesGallery from "../../components/main/ImagesGallery/ImagesGallery";
 
+import styles from '../Views.module.scss'
 
 
 const DocumentaryView = () => {
   return (
-    <>
+    <div className={styles.views}>
+    <Navigation />
     <ImagesGallery/>
-    </>
+    </div>
   );
 };
 
--- src/views/NatureView/NatureView.js
+++ src/views/NatureView/NatureView.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Navigation from "../../components/main/Navigation";
 import ImagesGallery from "../../components/main/ImagesGallery/ImagesGallery";
+
+import styles from '../Views.module.scss'
 
 
 
 const NatureView = () => {
   return (
-    <>
+    <div className={styles.views}>
+    <Navigation />
     <ImagesGallery/>
-    </>
+    </div>
   );
 };
 
