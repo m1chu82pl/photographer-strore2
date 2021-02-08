433081f2d1ab5b44c0244f561206f44e2ed8b283
m1chu82pl <criminal.industries@gmail.com>
2021-02-08 11:03:53 +0100

    bug with environmetn variable

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -3,13 +3,15 @@ import React from "react";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
-const initialDate = new Date();
-const year = initialDate.getFullYear();
+  const initialDate = new Date();
+  const year = initialDate.getFullYear();
+  
+    console.log("environment from .env file: ", process.env);
   return (
     <footer className={styles.footer}>
       <h6>© 2020 - {year} MONKEY FINGER // CREATIVE DIRECTOR-DEVELOPER</h6>
       <h6>All Rights Reserved ™</h6>
-      <h6 className={styles.version}>🆚 0.0.1</h6>
+      <h6 className={styles.version}>🆚 </h6>
     </footer>
   );
 };
