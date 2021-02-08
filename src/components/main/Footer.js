f3c9bc8975de9e5b00f7f8aea74757c83bcf23f1
m1chu82pl <criminal.industries@gmail.com>
2021-02-08 11:56:16 +0100

    add version number in .env file

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -6,12 +6,11 @@ const Footer = () => {
   const initialDate = new Date();
   const year = initialDate.getFullYear();
   
-    console.log("environment from .env file: ", process.env);
   return (
     <footer className={styles.footer}>
       <h6>© 2020 - {year} MONKEY FINGER // CREATIVE DIRECTOR-DEVELOPER</h6>
       <h6>All Rights Reserved ™</h6>
-      <h6 className={styles.version}>🆚 </h6>
+      <h6 className={styles.version}>🆚 {process.env.REACT_APP_VERSION}</h6>
     </footer>
   );
 };
