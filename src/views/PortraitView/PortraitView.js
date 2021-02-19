08d9e3d30df75888256215426c96bf4572c1b1a0
m1chu82pl <criminal.industries@gmail.com>
2021-02-19 12:46:30 +0100

    add styled compts to Views

--- src/views/PortraitView/PortraitView.js
+++ src/views/PortraitView/PortraitView.js
@@ -2,14 +2,14 @@ import React from "react";
 import Navigation from "../../components/main/Navigation/Navigation";
 import ImagesGallery from "../../components/main/ImagesGallery/ImagesGallery";
 
-import styles from '../Views.module.scss';
+import Views from "../Views.styled.js";
 
 const PortraitView = () => {
   return (
-    <div className={styles.views}>
-    <Navigation />
-    <ImagesGallery/>
-    </div>
+    <Views>
+      <Navigation />
+      <ImagesGallery />
+    </Views>
   );
 };
 
